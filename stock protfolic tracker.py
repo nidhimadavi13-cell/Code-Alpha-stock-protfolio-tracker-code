e876bca70c94c0efe2bc004160b9d1b3d1b686fb
nidhimadavi13-cell <nidhimadavi13@gmail.com>
2026-06-05 10:08:32 +0530

    Create stock protfolic tracker.py

---
+++ stock protfolic tracker.py
@@ -0,0 +1,12 @@
+stocks={
+"TCS":3500,
+"INFY":1500,
+"WIPRO":500
+}
+stock= input("Enter stock name:").upper()
+quantity = int(input("Enter quantity: "))
+if stock in stocks:
+     total=stocks[stock]*quantity
+     print("Total Investment value=",total)
+else:
+         print("Stock not found")
